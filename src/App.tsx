4e6ea1f37ced070bd6b9eaabbb76644fbfda5ce1
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2024-06-11 15:51:45 +0500

    sign-up-page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
+import Signup from './components/Signup';
+function App() {
+
+  return (
+    <>
+      <div className="body">
+      <Signup/>
+    </div>
+    </>
+  )
+}
+
+export default App
